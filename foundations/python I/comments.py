29fc58f55924b6c679f1c1fb2a7c16ffc1e277b6
Matías Rodlo <m@hablaqui.com>
2024-08-06 23:34:43 -0400

    floating and integers

--- foundations/python I/comments.py
+++ foundations/python I/comments.py
@@ -24,3 +24,11 @@ meal = "Bolognesa"
 # Printing out dinner
 print("Dinner:")
 print(meal)
+
+# Define the release and runtime integer variables below:
+release_year = 2024
+runtime = 5
+
+
+# Define the rating_out_of_10 float variable below:
+rating_out_of_10 = 3.2
