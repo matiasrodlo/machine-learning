cc2fbb574e75634b32511fef5eea5dfdb8a8a5f1
Matías Rodlo <m@hablaqui.com>
2024-08-06 23:20:21 -0400

    variables in python

--- foundations/python I/comments.py
+++ foundations/python I/comments.py
@@ -3,3 +3,24 @@ persnickety_count = 0
 
 # printing hello world
 print("Hello world")
+
+# Variables
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Burger"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Bolognesa"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
