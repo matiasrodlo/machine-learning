6ec0107d4bf02fb90193e5057fb35aa8a6c58e55
Matías Rodlo <m@hablaqui.com>
2024-08-17 10:55:34 -0600

    concatenate strings

--- foundations/python-I/comments.py
+++ foundations/python-I/comments.py
@@ -56,3 +56,29 @@ print(7 ** 2)
 print(8 ** 2)
 
 print(6 ** 4)
+
+# Modulo
+
+order_263_r = 263 % 11
+print(order_263_r)
+
+order_263_coupon = "no"
+
+order_264_r = 264 % 11
+print(order_264_r)
+
+order_264_coupon = "yes"
+
+# Concatenate
+
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+print(message)
