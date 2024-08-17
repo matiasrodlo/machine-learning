b509e7ee3368f0788d47e33c9781b571b88721f3
Matías Rodlo <m@hablaqui.com>
2024-08-17 11:08:42 -0600

    plus add python

--- foundations/python-I/comments.py
+++ foundations/python-I/comments.py
@@ -82,3 +82,20 @@ string6 = "from which it wafted the most delightful scent of flowers and hay."
 message = string1 + string2 + string3 + string4 + string5 + string6
 
 print(message)
+
+# Plus equal
+
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price += nice_sweater
+total_price += fun_books
+
+print("The total price is", total_price)
