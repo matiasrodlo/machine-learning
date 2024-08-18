85364cbd76f349c6fb855007e46d495fff1f8206
Matías Rodlo <m@hablaqui.com>
2024-08-18 12:07:50 -0600

    build function in python

--- foundations/python-I/travel.py
+++ foundations/python-I/travel.py
@@ -74,3 +74,18 @@ trip_planner("France", "Germany", "Denmark")
 trip_planner("Denmark", "France", "Germany")
 trip_planner(first_destination="Iceland", final_destination="Germany", second_destination="India")
 trip_planner("Brooklyn", "Queens")
+
+# Build in function in python
+
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+min_price = min(tshirt_price, shorts_price, mug_price, poster_price)
+rounded_price = round(tshirt_price, 1)
+print(max_price)
+print(min_price)
+print(rounded_price)
