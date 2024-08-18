71b17b8fe6e5e6c6687aea200ecc918006a87078
Matías Rodlo <m@hablaqui.com>
2024-08-18 00:19:59 -0600

    defining function

--- foundations/python-I/travel.py
+++ foundations/python-I/travel.py
@@ -17,3 +17,10 @@ print("The best route is by train and will take approximately 10 minutes.")
 print("Setting the Empire State Building as the starting point and Times Square as our destination.")
 print("Calculating the total distance between our points.") 
 print("The best route is by train and will take approximately 10 minutes.") 
+
+# Defining a function
+
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station")
+  print("Take the Northbound N, Q, R, or W train 1 stop")
+  print("Get off the Times Square 42nd Street stop")
