337758938eaf7a140cf3cf7a253d33d554186e61
Matías Rodlo <m@hablaqui.com>
2024-08-20 13:16:38 -0600

    if statement

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -18,3 +18,13 @@ print(type(my_baby_bool))
 
 my_baby_bool_two = True
 print(type(my_baby_bool_two))
+
+# If Statement
+
+user_name = "angela_catlady_87"
+
+if user_name == "Dave":
+  print("Get off my computer Dave!")
+if user_name == "angela_catlady_87":
+  print("I know it is you, Dave! Go away!")
+
