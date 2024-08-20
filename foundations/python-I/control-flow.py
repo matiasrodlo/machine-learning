39ee39098cee6ab4f9f8e70bdd81862788177fd0
Matías Rodlo <m@hablaqui.com>
2024-08-20 12:54:11 -0600

    relational operators

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -4,3 +4,9 @@ statement_one = "No"
 statement_two = "Yes"
 statement_three = "No"
 statement_four = "Yes"
+
+# relational operators:
+
+first_expression = True
+second_expression = True
+third_expression = False
