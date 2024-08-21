292d4af0a7552095b5459c41f5aac536960c5cf2
Matías Rodlo <m@hablaqui.com>
2024-08-21 00:52:45 -0600

    if statements

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -28,3 +28,19 @@ if user_name == "Dave":
 if user_name == "angela_catlady_87":
   print("I know it is you, Dave! Go away!")
 
+# Relational Operators II
+
+x = 20
+y = 20
+
+# Write the first if statement here:
+if x == y:
+  print("These numbers are the same")
+
+
+credits = 120
+
+# Write the second if statement here:
+if credits >= 120:
+  print("You have enough credits to graduate!")
+
