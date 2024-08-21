f56798a99b87d9dd6a9db88d13a3f03fe1b52870
Matías Rodlo <m@hablaqui.com>
2024-08-21 00:59:20 -0600

    boolean operator and

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -44,3 +44,14 @@ credits = 120
 if credits >= 120:
   print("You have enough credits to graduate!")
 
+# Boolean Operators: and
+
+statement_one = False
+
+statement_two = True
+
+credits = 120
+gpa = 3.4
+
+if credits >= 120 and gpa >= 2.0:
+  print("You meet the requirements to graduate!")
