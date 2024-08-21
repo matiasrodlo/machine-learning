7c9668e727ddda88ed6541e9896fd8fad859448d
Matías Rodlo <m@hablaqui.com>
2024-08-21 01:23:44 -0600

    boolean operator or

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -55,3 +55,15 @@ gpa = 3.4
 
 if credits >= 120 and gpa >= 2.0:
   print("You meet the requirements to graduate!")
+
+# Boolean Operators: or
+
+statement_one = True
+
+statement_two = True
+
+credits = 118
+gpa = 2.0
+
+if credits >= 120 or gpa >= 2.0:
+  print("You have met at least one of the requirements.")
