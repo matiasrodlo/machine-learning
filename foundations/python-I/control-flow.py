322aec7850d4ee24e952ba666246e0cb9338338a
Matías Rodlo <m@hablaqui.com>
2024-08-24 10:09:57 -0600

    not operants

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -67,3 +67,19 @@ gpa = 2.0
 
 if credits >= 120 or gpa >= 2.0:
   print("You have met at least one of the requirements.")
+
+# Boolean Operators: not
+
+statement_one = False
+
+statement_two = True
+
+credits = 120
+gpa = 1.8
+
+if not credits >= 120:
+  print("You do not have enough credits to graduate.")
+if not gpa >= 2.0:
+  print("Your GPA is not high enough to graduate.")
+if not credits >= 120 and not gpa >= 2.0:
+  print("You do not meet either requirement to graduate!")
