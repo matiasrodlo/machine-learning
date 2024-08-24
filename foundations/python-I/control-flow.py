f30833d777cf6e91464c0d81cfd5a2f7ac0271fe
Matías Rodlo <m@hablaqui.com>
2024-08-24 10:36:24 -0600

    elif statement

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -94,3 +94,17 @@ if (credits >= 120) and (gpa >= 2.0):
 else:
   print("You do not meet the requirements to graduate.")
 
+# Elif statement
+
+grade = 86
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")
+else:
+  print("F")
