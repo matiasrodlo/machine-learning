2f303e5a87864b0f53f34ef312b696a3733a2ba3
Matías Rodlo <m@hablaqui.com>
2024-08-24 11:16:20 -0600

    control flow review

--- foundations/python-I/control-flow.py
+++ foundations/python-I/control-flow.py
@@ -108,3 +108,20 @@ elif grade >= 60:
   print("D")
 else:
   print("F")
+
+# Flow control review
+
+if planet == 1:
+  weight = weight * 0.91
+elif planet == 2:
+  weight = weight * 0.38
+elif planet == 3:
+  weight = weight * 2.34
+elif planet == 4:
+  weight = weight * 1.06
+elif planet == 5:
+  weight = weight * 0.92
+elif planet == 6:
+  weight = weight * 1.19
+
+print("Your weight:", weight)
