7110c64a7c0b78e473cf315952b66c482002cb9f
Matías Rodlo <m@hablaqui.com>
2024-08-24 11:33:22 -0600

    list elements

--- foundations/python-I/list.py
+++ foundations/python-I/list.py
@@ -3,3 +3,9 @@
 heights = [61, 70, 67, 64, 65]
 
 broken_heights = [65, 71, 59, 62]
+
+# List elements
+
+ints_and_strings = [1, 2, 3, "four", "five", "hola"]
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
