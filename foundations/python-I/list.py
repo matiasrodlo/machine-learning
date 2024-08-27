64dffa14280b5a0c9637a613e0c89807e62d9fd8
Matías Rodlo <m@hablaqui.com>
2024-08-26 23:46:32 -0600

    accesing 2d list

--- foundations/python-I/list.py
+++ foundations/python-I/list.py
@@ -80,3 +80,14 @@ new_store_order_list.remove("Onions")
 heights = [["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64], ["Vik", 68]]
 
 ages = [["Aaron", 15], ["Dhruti", 16]]
+
+# Accessing 2D Lists
+
+class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83],  ["Ellie", 101.5]]
+print(class_name_test)
+
+sams_score = class_name_test[2][1]
+print(sams_score)
+
+ellies_score = class_name_test[-1][-1]
+print(ellies_score)
