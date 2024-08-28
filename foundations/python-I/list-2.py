fd2cb4e1f905cfdc6b0daae434e2c610d8f170e1
Matías Rodlo <m@hablaqui.com>
2024-08-27 21:08:19 -0600

    pop python list

--- foundations/python-I/list-2.py
+++ foundations/python-I/list-2.py
@@ -4,3 +4,16 @@ front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
 print(front_display_list)
 
 front_display_list.insert(0, "Pineapple")
+
+# Removing by Index: Pop
+
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Your code below: 
+
+data_science_topics.pop()
+print(data_science_topics)
+
+data_science_topics.pop(3)
+print(data_science_topics)
