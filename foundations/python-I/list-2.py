f5d2a2d1e6f7f0a4e2a17a58c7e337f5f715522c
Matías Rodlo <m@hablaqui.com>
2024-08-27 22:40:56 -0600

    counting in a list

--- foundations/python-I/list-2.py
+++ foundations/python-I/list-2.py
@@ -62,3 +62,9 @@ print(last_two_elements)
 slice_off_last_three = suitcase[:-3]
 print(slice_off_last_three)
 
+# Counting in a list: 
+
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+jake_votes = votes.count("Jake")
+print(jake_votes)
