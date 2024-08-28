97080b4e003f3617adc25824977c4a3017c1e869
Matías Rodlo <m@hablaqui.com>
2024-08-27 21:23:07 -0600

    list range of numbers

--- foundations/python-I/list-2.py
+++ foundations/python-I/list-2.py
@@ -17,3 +17,10 @@ print(data_science_topics)
 
 data_science_topics.pop(3)
 print(data_science_topics)
+
+# List Range: 
+number_list = range(0,9)
+print(list(number_list))
+
+zero_to_seven = range(0,8)
+print(list(zero_to_seven))
