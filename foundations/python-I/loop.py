77cb694a59b7cfb87ac706e0a6f66fcc2fe306c9
Matías Rodlo <m@hablaqui.com>
2024-08-31 17:35:39 -0600

    loop break

--- foundations/python-I/loop.py
+++ foundations/python-I/loop.py
@@ -68,3 +68,14 @@ students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
 
 for student in students_period_A:
   print(student)
+
+  # Loop Control: Break
+
+  dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
+  if dog_breed == dog_breed_I_want:
+    print("They have the dog I want!") 
+    break
