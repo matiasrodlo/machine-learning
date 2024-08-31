bd09324df09b76bae3d244aeb0fe2d83144c74ee
Matías Rodlo <m@hablaqui.com>
2024-08-31 14:08:54 -0600

    while loop list

--- foundations/python-I/loop.py
+++ foundations/python-I/loop.py
@@ -50,3 +50,13 @@ while countdown >= 0:
   print(countdown)
   countdown -= 1
 print("We have liftoff!")
+
+# While Loops: Lists: 
+
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+while index < length:
+  print("I am learning about " + python_topics[index])
+  index += 1
