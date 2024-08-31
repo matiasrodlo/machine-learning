413cc4a482d11bae5852cc82e60c5279b3b4927a
Matías Rodlo <m@hablaqui.com>
2024-08-31 11:14:38 -0600

    for loop induction

--- foundations/python-I/loop.py
+++ foundations/python-I/loop.py
@@ -10,3 +10,15 @@ print("This can be so much easier with loops!")
 print("This can be so much easier with loops!")
 print("This can be so much easier with loops!")
 print("This can be so much easier with loops!")
+
+# For loop induction
+
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+  print(game)
+
+for sports in sport_games:
+  print(sports)
