d6d4d0ee748c8c7e07e8dec2fc57eb62a737157b
Matías Rodlo <m@hablaqui.com>
2024-08-31 18:00:29 -0600

    nested loops

--- foundations/python-I/loop.py
+++ foundations/python-I/loop.py
@@ -88,3 +88,16 @@ for i in ages:
   if i < 21:
     continue
   print(i)
+
+# Nested Loops
+
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0;
+
+for location in sales_data:
+  print(location)
+  for elements in location:
+    scoops_sold += elements
+
+print(scoops_sold)
