c39ad721d5c2ec7f04a05665f48d8050f6b03670
Matías Rodlo <m@hablaqui.com>
2024-09-04 00:06:48 -0600

    concatenating string

--- foundations/python-II/string.py
+++ foundations/python-II/string.py
@@ -14,3 +14,16 @@ last_name = "Villanueva"
 
 new_account = last_name[:5]
 temp_password = last_name[2:6]
+
+# Concatenating strings
+
+first_name = "Julie"
+last_name = "Blevins"
+
+def account_generator(first_name, last_name):
+  account_name = first_name[:3] + last_name[:3]
+  return account_name
+
+new_account = account_generator(first_name, last_name)
+
+print(new_account)
