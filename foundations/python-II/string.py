fb350b874e9bc6a40122ee7ea87cbe5820ce67c0
Matías Rodlo <m@hablaqui.com>
2024-09-05 14:05:32 -0600

    string conditionals II

--- foundations/python-II/string.py
+++ foundations/python-II/string.py
@@ -75,3 +75,14 @@ def letter_check(word, letter):
       return True
   return False
 
+# Strings and Conditionals (Part Two)
+
+def contains(big_string, little_string):
+  return little_string in big_string
+
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
