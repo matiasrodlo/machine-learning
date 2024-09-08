37f78838f788a30346bb4b8d375f6f734452c1c8
Matías Rodlo <m@hablaqui.com>
2024-09-08 11:43:49 -0600

    split string II

--- foundations/python-II/string.py
+++ foundations/python-II/string.py
@@ -124,3 +124,17 @@ print(poem_author_fixed)
 line_one = "The sky has given over"
 
 line_one_words = line_one.split()
+
+# Split String II
+
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+  
+print(author_last_names)
