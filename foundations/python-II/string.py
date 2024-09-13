f4383ca985070fc055cadb4a25f1d6669dcaacd9
Matías Rodlo <m@hablaqui.com>
2024-09-13 12:44:25 -0600

    strip method

--- foundations/python-II/string.py
+++ foundations/python-II/string.py
@@ -175,3 +175,15 @@ winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the
 winter_trees_full = '\n'.join(winter_trees_lines)
 
 print(winter_trees_full)
+
+# Strip()
+
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+love_maybe_lines_stripped = []
+
+for line in love_maybe_lines:             
+   love_maybe_lines_stripped.append(line.strip())
+
+love_maybe_full = '\n'.join(love_maybe_lines_stripped)
+print(love_maybe_full)
