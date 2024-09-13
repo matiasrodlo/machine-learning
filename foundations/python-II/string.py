30e5af65059351f7a73c56d30c24ab46a8c562d8
Matías Rodlo <m@hablaqui.com>
2024-09-13 12:31:06 -0600

    join string II

--- foundations/python-II/string.py
+++ foundations/python-II/string.py
@@ -167,3 +167,11 @@ spring_storm_lines = spring_storm_text.split('\n')
 reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
 
 reapers_line_one = ' '.join(reapers_line_one_words)
+
+# Join() string II 
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = '\n'.join(winter_trees_lines)
+
+print(winter_trees_full)
