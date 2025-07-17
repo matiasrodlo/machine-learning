e4ae28c87540b285af1633a0bf158dd63458ac2f
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-07-17 01:33:09 -0400

    Update dictionaries.py

--- foundations/python-II/dictionaries.py
+++ foundations/python-II/dictionaries.py
@@ -2,3 +2,50 @@ sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 
 print(sensors)
+
+# Make a Dictionary
+
+translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"} 
+
+# Invalid Key
+
+children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+# Empty Dictionary
+
+my_empty_dictionary = {}
+
+# Add a key
+
+animals_in_zoo = {}
+
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+
+print(animals_in_zoo)
+
+# Add multiple keys
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+
+print(user_ids)
+
+# Overwrite Values
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+oscar_winners["Supporting Actress"] = "Viola Davis"
+
+oscar_winners["Best Picture"] = "Moonlight"
+
+# Dictionary Comprehension
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks = zip(drinks, caffeine)
+
+drinks_to_caffeine = {key: value for key, value in zipped_drinks}
