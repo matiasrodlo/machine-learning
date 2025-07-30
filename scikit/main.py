7ab0567a0ca5b09df949d5d7bfe512d42b9bbd37
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-07-29 23:39:38 -0400

    Update main.py

--- scikit/main.py
+++ scikit/main.py
@@ -17,3 +17,47 @@ import seaborn as sns
 
 data = pd.read_csv("titanic.csv")
 data.info()
+print(data.isnull().sum())
+
+# Data cleaning and feature engineering
+def preprocess_data(df):
+    df.drop(columns=["PassengerId", "Name", "Ticket", "Cabin"], inplace=True)
+
+    df["Embarked"].fillna("S", inplace=True)
+    df.drop(columns=["Embarked"], inplace=True)
+
+    fill_missing_ages(df)
+
+# Convert Gender
+    df["Sex"] = df["Sex"].map({'male':1, "female":0})
+
+# Feature Enginnering
+
+    df["FamilySize"] = df["SibSp"] + df["Parch"]
+    df["IsAlone"] = np.where(df["FamilySize"] == 0, 1, 0)
+    df["FareBin"] = pd.qcut(df["Fare"], 4, labels=False)
+    df["AgeBin"] = pd.cut(df["age"], bins=[0,12,20,40,60, np.inf], labels=False)
+
+    return df
+
+# Fill in missing ages
+def fill_missing_ages(df):
+    age_fill_map = {}
+    for pclass in df["Pclass"].unique():
+        if pclass not in age_fill_map:
+            age_fill_map[pclass] = df[df["Pclass"] == pclass]["Age"].median()
+
+    df["Age"] = df.apply(lambda row: age_fill_map[row["Pclass"]] if pd.isnull(row["Age"]) else row["age"], axis=1)
+
+data = preprocess_data(data)
+
+X = data.drop(columns=["Survived"])
+Y=data["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# ML Preprocessing
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
