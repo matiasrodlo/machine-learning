e8c16d5c170a786ae1cb06a21f9ce2635dc087de
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-07-29 22:25:22 -0400

    Create main.py

---
+++ scikit/main.py
@@ -0,0 +1,9 @@
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("titanic.csv")
