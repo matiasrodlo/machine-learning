d1be3721d9d082f9c86f046c373a8d843d78b34b
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-08-08 01:04:53 -0400

    Create 4-any-non-zero.py

---
+++ numpy/exercises/4-any-non-zero.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x = np.array([1, 0, 0, 0])
+
+print("Original array:")
+print(x)
+
+print("Test whether any of the elements of a given array is non-zero:")
+print(np.any(x))
+
+x = np.array([0, 0, 0, 0])
+
+print("Original Array")
+print(x)
+
+print("test wheather any of the elements of a given array is a non-zero")
+print(np.any(x))
