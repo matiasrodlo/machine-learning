99e2d14520ab7d236f4202e260b2e2f0c9673869
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-08-08 23:56:57 -0400

    Create 8-comparison.py

---
+++ numpy/exercises/8-comparison.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x = np.array([3, 5])
+y = np.array([2, 5])
+
+print("Original numbers: ")
+print(x)
+print(y)
+
+print("Comparison - greater")
+print(np.greater_equal(x, y))
+
+print("Comparison - less")
+print(np.less(x, y))
+
+print("Comparison - less_equal")
+print(np.less_equal(x, y))
